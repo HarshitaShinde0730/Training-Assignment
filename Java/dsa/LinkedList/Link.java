0aea0a8708b43520e68c6d77a906b00193ef9145
Harshita Shinde <178906380+HarshitaShinde0730@users.noreply.github.com>
2025-01-03 12:16:05 +0530

    Add files via upload

---
+++ Java/dsa/LinkedList/Link.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+public class Link {
+    Node head;
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+    public static Link insert(Link list, int data) {
+        Node new_node = new Node(data);
+        if (list.head == null) {
+            list.head = new_node;
+        } else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new_node;
+        }
+        return list;
+    }
+    public static void printList(Link list) {
+        Node currNode = list.head;
+
+        System.out.print("LinkedList: ");
+
+        while (currNode != null) {
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Link list = new Link();
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+        list = insert(list, 6);
+        list = insert(list, 7);
+        list = insert(list, 8);
+
+        // Print the linked list
+        printList(list);
+    }
+}
