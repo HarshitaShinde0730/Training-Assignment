691bde65acac1985c01aa46433190f10e0b0076b
Harshita Shinde <178906380+HarshitaShinde0730@users.noreply.github.com>
2025-01-06 12:55:17 +0530

    Add files via upload

---
+++ Java/CollectionFramework/Collectionstack.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+
+public class Collectionstack {
+    public static void main(String[] args) {
+        Stack<Integer> arr = new Stack<>();
+        arr.push(10);
+        arr.push(20);
+        System.out.println("Stack: " + arr);
+    }
+}
