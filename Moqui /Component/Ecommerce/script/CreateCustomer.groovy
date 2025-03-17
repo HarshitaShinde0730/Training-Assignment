c65c0a3f48aeeb551f579535caee92ee2f7c0fa1
harshita <harshita.shinde@hotwax.co>
2025-03-17 12:21:02 +0530

    commit Ecommerce

---
+++ Moqui /Component/Ecommerce/script/CreateCustomer.groovy
@@ -0,0 +1,105 @@
+// Importing required packages for implementations
+import org.moqui.context.ExecutionContext
+import org.moqui.entity.EntityCondition // To implement condition
+import org.moqui.entity.EntityFind // To implement find
+import org.moqui.entity.EntityValue // To access a single record in the database
+
+ExecutionContext ec =  context.ec // Getting the execution context
+
+// Checking if our email address already exist in contact mech entity
+EntityFind ef_cm = ec.entity.find('ecommerce.ContactMech')
+
+if (emailAddress){
+    ef_cm.condition("infoString", EntityCondition.EQUALS, emailAddress)
+}
+EntityValue ev_cm = ef_cm.one()
+if (ev_cm){
+    ec.message.addError("Record already exists")
+}else{
+    //    If email address doesn't exist
+
+    //    Creating a record in the party entity
+    String partyId = ec.entity.sequencedIdPrimary("ecommerce.Party", null, null)
+    partyId = 'PER' + partyId
+    EntityValue ev_pe = ec.entity.makeValue('ecommerce.Party')
+    ev_pe.set('partyId', partyId)
+    ev_pe.set('partyTypeEnumId', 'PtyPerson')
+    ev_pe.create()
+
+    // Creating a record in the person entity
+    EntityValue ev_per = ec.entity.makeValue('ecommerce.Person')
+    ev_per.set('partyId', partyId)
+    ev_per.set('firstName', firstName)
+    ev_per.set('lastName', lastName)
+    ev_per.create()
+
+    //    Creating a record in party role
+    EntityValue ev_pr = ec.entity.makeValue('ecommerce.PartyRole')
+    ev_pr.set('partyId', partyId)
+    ev_pr.set('roleTypeId', 'CUSTOMER')
+    ev_pr.create()
+
+    // Creating email address
+    EntityValue ev_cm_1 = ec.entity.makeValue('ecommerce.ContactMech')
+    def contactMechId1 = ec.entity.sequencedIdPrimary('ecommerce.ContactMech', null, null)
+    ev_cm_1.set('contactMechId', contactMechId1)
+    ev_cm_1.set('contactMechTypeEnumId', 'ctyEmailAddress')
+    ev_cm_1.set('infoString', emailAddress)
+    ev_cm_1.create()
+
+    EntityValue ev_pcm_1 = ec.entity.makeValue('ecommerce.PartyContactMech')
+    ev_pcm_1.set('partyId', partyId)
+    ev_pcm_1.set('contactMechId', contactMechId1)
+    ev_pcm_1.set('contactMechPurposeId', 'PRIMARY_EMAIL')
+    ev_pcm_1.set('fromDate', System.currentTimeMillis())
+    ev_pcm_1.create()
+
+    // --------
+    // Creating postal address
+    EntityValue ev_cm_2 = ec.entity.makeValue('ecommerce.ContactMech')
+    def contactMechId2 = ec.entity.sequencedIdPrimary('ecommerce.ContactMech', null, null)
+    ev_cm_2.set('contactMechId', contactMechId2)
+    ev_cm_2.set('contactMechTypeEnumId', 'ctyPostalAddress')
+    ev_cm_2.create()
+
+    EntityValue postalAddress = ec.entity.makeValue('ecommerce.PostalAddress')
+    postalAddress.set('contactMechId', contactMechId2)
+    postalAddress.set('address1', address1)
+    if (address2){
+        postalAddress.set('address2', address2)
+    }
+    postalAddress.set('city', city)
+    postalAddress.set('postalCode', postalCode)
+    postalAddress.create()
+
+    EntityValue ev_pcm_2 = ec.entity.makeValue('ecommerce.PartyContactMech')
+    ev_pcm_2.set('partyId', partyId)
+    ev_pcm_2.set('contactMechId', contactMechId2)
+    ev_pcm_2.set('contactMechPurposeId', 'PRIMARY_LOCATION')
+    ev_pcm_2.set('fromDate', System.currentTimeMillis())
+    ev_pcm_2.create()
+
+    // Creating Telecom Number
+    if (contactNumber.length() > 10 || contactNumber.length() < 10){
+        ec.message.addError("Invalid contact number. It must be 10 digits.")
+    }
+    EntityValue ev_cm_3 = ec.entity.makeValue('ecommerce.ContactMech')
+    def contactMechId3 = ec.entity.sequencedIdPrimary('ecommerce.ContactMech', null, null)
+    ev_cm_3.set('contactMechId', contactMechId3)
+    ev_cm_3.set('contactMechTypeEnumId', 'ctyTelecomNumber')
+    ev_cm_3.create()
+
+    EntityValue telecomNumber = ec.entity.makeValue('ecommerce.TelecomNumber')
+    telecomNumber.set('contactMechId', contactMechId3)
+    telecomNumber.set('countryCode', countryCode)
+    telecomNumber.set('areaCode', areaCode)
+    telecomNumber.set('contactNumber', contactNumber)
+    telecomNumber.create()
+
+    EntityValue ev_pcm_3 = ec.entity.makeValue('ecommerce.PartyContactMech')
+    ev_pcm_3.set('partyId', partyId)
+    ev_pcm_3.set('contactMechId', contactMechId3)
+    ev_pcm_3.set('contactMechPurposeId', 'PRIMARY_PHONE')
+    ev_pcm_3.set('fromDate', System.currentTimeMillis())
+    ev_pcm_3.create()
+}
---
+++ Moqui /Component/Ecommerce/script/FindCustomer.groovy
@@ -0,0 +1,114 @@
+// Importing required packages for implementations
+import org.moqui.context.ExecutionContext
+import org.moqui.entity.EntityCondition // To implement condition
+import org.moqui.entity.EntityFind // To implement find
+import org.moqui.entity.EntityList //To get the list of EntityValue objects
+import org.moqui.entity.EntityValue // To access a single record in the database
+
+
+ec.logger.info("-----Service was called-----")
+ExecutionContext ec =  context.ec // Getting the execution context
+
+// Finding our Customer View entity
+EntityFind ef = ec.entity.find('ecommerce.FindCustomerView')
+
+ef.selectField("partyId")
+
+// Getting all list of records based on partyId
+//if (partyId) {
+//    ef.condition(
+//            ec.entity.conditionFactory.makeCondition(
+//                    "partyId", EntityCondition.LIKE, (leadingWildcard ? "%" : "") + partyId + "%")
+//                    .ignoreCase())
+//}
+
+if (firstName) {
+    ef.condition(
+            ec.entity.conditionFactory.makeCondition(
+                    "firstName", EntityCondition.LIKE, (leadingWildcard ? "%" : "") + firstName + "%")
+                    .ignoreCase())
+}
+if (lastName) {
+    ef.condition(
+            ec.entity.conditionFactory.makeCondition(
+                    "lastName", EntityCondition.LIKE, (leadingWildcard ? "%" : "") + lastName + "%")
+                    .ignoreCase())
+}
+
+if (emailAddress){
+    ef.condition(
+            ec.entity.conditionFactory.makeCondition(
+                    "infoString", EntityCondition.LIKE, (leadingWildcard ? "%" : "") + emailAddress + "%")
+                    .ignoreCase())
+}
+
+if (countryCode){
+    ef.condition(
+            ec.entity.conditionFactory.makeCondition(
+                    "countryCode", EntityCondition.LIKE, (leadingWildcard ? "%" : "") + countryCode + "%")
+                    .ignoreCase())
+}
+
+
+if (areaCode){
+    ef.condition(
+            ec.entity.conditionFactory.makeCondition(
+                    "areaCode", EntityCondition.LIKE, (leadingWildcard ? "%" : "") + areaCode + "%")
+                    .ignoreCase())
+}
+
+if (contactNumber){
+    ef.condition(
+            ec.entity.conditionFactory.makeCondition(
+                    "contactNumber", EntityCondition.LIKE, (leadingWildcard ? "%" : "") + contactNumber + "%")
+                    .ignoreCase())
+}
+
+if (address1){
+    ef.condition(ec.entity.conditionFactory.makeCondition("address1", EntityCondition.LIKE,
+            (leadingWildcard ? "%":"") + address1 + "%"
+    ))
+}
+
+if (address2){
+    ef.condition(ec.entity.conditionFactory.makeCondition("address1", EntityCondition.LIKE,
+            (leadingWildcard ? "%":"") + address2 + "%"
+    ))
+}
+
+if (city){
+    ef.condition(
+            ec.entity.conditionFactory.makeCondition(
+                    "city", EntityCondition.LIKE, (leadingWildcard ? "%" : "") + city + "%")
+                    .ignoreCase())
+}
+
+if (postalCode){
+    ef.condition(
+            ec.entity.conditionFactory.makeCondition(
+                    "postalCode", EntityCondition.LIKE, (leadingWildcard ? "%" : "") + postalCode + "%")
+                    .ignoreCase())
+}
+
+//def combinedName = firstName + lastName
+//ef.orderBy(combinedName)
+
+EntityList el = ef.list() // Getting the list of EntityValue objects
+
+// Initialize an empty list to store the partyId values
+partyIdList = []
+
+// Iterate over the EntityList and add partyId values to the list
+partyIdList.add(el)
+
+if (!pageNoLimit) { ef.offset(pageIndex as int, pageSize as int); ef.limit(pageSize as int) }
+
+
+ec.logger.info("-----------------------------" + el + "-------------------------")
+partyIdListCount = ef.count()
+partyIdListPageIndex = ef.pageIndex
+partyIdListPageSize = ef.pageSize
+partyIdListPageMaxIndex = ((BigDecimal) (partyIdListCount - 1)).divide(partyIdListPageSize, 0, BigDecimal.ROUND_DOWN) as int
+partyIdListPageRangeLow = partyIdListPageIndex * partyIdListPageSize + 1
+partyIdListPageRangeHigh = (partyIdListPageIndex * partyIdListPageSize) + partyIdListPageSize
+if (partyIdListPageRangeHigh > partyIdListCount) partyIdListPageRangeHigh = partyIdListCount
